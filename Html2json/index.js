c93543d879d6788aaafda2001372b53de68e8a17
ozawako1 <ozawako1@gmail.com>
2020-05-20 23:01:04 +0900

    2json init

--- Html2json/index.js
+++ Html2json/index.js
@@ -1,7 +1,8 @@
-﻿//index.js
+﻿
 var t2j = require('tabletojson').Tabletojson;
 var j2c  = require('json2csv');
 var cherio = require('cheerio');
+
 
 module.exports = async function (context, req) {
 
@@ -11,17 +12,18 @@ module.exports = async function (context, req) {
 
     try {
         
-        var html = req.body.html;
+        var html = req.body;
         if (html == undefined) {
             throw new Error("Invalid Arg.");
-        }   
+        }
 
         var answer = [];
         var fin = {};
-
         var hobj = cherio.load(html, { decodeEntities: false });
-        var list = hobj("strong");
-        var list = list.nextAll("table");
+
+        // Pardotメール固有のParse
+        var contents = hobj("strong");
+        var list = contents.nextAll("table");
 
         for (var i = 0 ; i < list.length ; i++) {
         
@@ -32,7 +34,6 @@ module.exports = async function (context, req) {
             
             var x = j.filter((itm) => (itm['項目'] != undefined));
             answer = answer.concat(x);
-
         }
         
         answer.forEach((itm) => {
@@ -40,11 +41,10 @@ module.exports = async function (context, req) {
             var val = itm['値'];
             fin[key] = val;
         });
-                
         csv = j2c.parse(fin, {withBOM: true});
         
     } catch (err) {
-
+        context.log(err.message);
         context.res = {        
             'status': 200,
             'content-type': 'text/plain',

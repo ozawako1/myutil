0995bc27323c3c69e488971289d96a60377f8889
ozawako1 <ozawako1@gmail.com>
2020-05-21 14:50:03 +0900

    more check

--- Html2json/index.js
+++ Html2json/index.js
@@ -9,6 +9,9 @@ module.exports = async function (context, req) {
     context.log('HTML2JSON HTTP trigger function processed a request.');
 
     var csv = "";
+    var body = "";
+    var tab = [];
+    var fin = {};
 
     try {
         
@@ -17,56 +20,63 @@ module.exports = async function (context, req) {
             throw new Error("Invalid Arg.");
         }
 
-        var answer = [];
-        var fin = {};
+        // 受信したHTMLをロード
         var hobj = cherio.load(html, { decodeEntities: false });
 
-        // Pardotメール固有のParse
+        // Pardotメール固有のParse1. <STRONG>タグの検索
         var contents = hobj("strong");
-        var list = contents.nextAll("table");
+        if (contents.length == 0) {
+            throw new Error("Could not find tag. Pardot Mail Format may changed.");
+        }
 
+        // Pardotメール固有のParse2. <TABLE>タグの検索
+        var list = contents.nextAll("table");
+        if (list.length == 0) {
+            throw new Error("Could not find tables. Pardot Mail Format may changed.");
+        }
+        
+        // 各テーブルについて列をjsonに
         for (var i = 0 ; i < list.length ; i++) {
         
             var itm = list[i];
             var jdata = t2j.convert(cherio.html(itm));
-        
+            if (jdata.length == 0){
+                throw new Error("JSON Convert error.");
+            }
+
             j = jdata[0];
             
             var x = j.filter((itm) => (itm['項目'] != undefined));
-            answer = answer.concat(x);
+            tab = tab.concat(x);
         }
         
-        answer.forEach((itm) => {
+        tab.forEach((itm) => {
             var key = itm['項目'];
             var val = itm['値'];
             fin[key] = val;
         });
         csv = j2c.parse(fin);
-        
+
+        body = iconv.encode(csv, "Windows932");
+
     } catch (err) {
+
         context.log(err.message);
-        context.res = {        
-            'status': 200,
-            'content-type': 'text/plain',
-            'body': err.message
-        };
-        context.done();
+        body = err.message;
 
     } finally {
-        
-        var body = iconv.encode(csv, "Windows932");
 
         context.res = {
             'status': 200,
-            'content-type': 'text/plain; charset=Windows932',
+            'content-type': 'text/plain',
             'body': body
         };
         context.done();
-
+/*
         var fs = require('fs');
         var FCSV =  "/Users/koichi.ozawa/Documents/development/out/d.csv";
         fs.writeFileSync(FCSV, body);
-
+*/
     }
 };
 

4b94e20dd369d4987a0016081cf07888c3115d9d
ozawako1 <ozawako1@gmail.com>
2020-05-25 17:15:03 +0900

    request dump

---
+++ dumprequest/index.js
@@ -0,0 +1,30 @@
+const fs = require('fs');
+
+const dump = process.env["MY_DUMP_FILE_PATH"];
+
+function getfilename()
+{
+    var now = new Date();
+    var name = "";
+    
+    name += "" + now.getFullYear() + now.getMonth() + now.getDate() + "Z";
+    name += "" + now.getHours() + now.getMinutes() + now.getSeconds();
+    name += ".json";
+
+    return name;
+}
+
+module.exports = async function (context, req) {
+    context.log('JavaScript HTTP trigger function processed a request.');
+
+    var buff = JSON.stringify(req);
+    var file = dump + "/" + getfilename();
+
+    fs.writeFileSync(file, buff);
+
+    const responseMessage = "";
+    context.res = {
+        // status: 200, /* Defaults to 200 */
+        body: responseMessage
+    };
+}
